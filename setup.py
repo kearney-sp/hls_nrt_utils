7ccf52ab2b983230a1db9ccdec511dd138a3a705
kearney-sp <spkearney@DESKTOP-F92HL6M.localdomain>
2022-05-06 09:37:34 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='hls_nrt_utils',
+      version='0.0.1',
+      description='PIP-installable utilities for performing near-real-time analyses using Harmonized Landsat-Sentinel dataset.',
+      author='Sean Patrick Kearney',
+      author_email='sean.patrick@hotmail.com',
+      license='MIT',
+      packages=['src'],
+      install_requires=['cartopy','certifi','cvxopt','dask','geopandas','numpy','pandas','rasterio','rioxarray','scikit-image','scikit-learn','scipy','stackstac','statsmodels','xarray','pysptools']
+    )
