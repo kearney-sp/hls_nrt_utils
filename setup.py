007cd686cf716743571e180ebaee7afcc88d37c9
kearney-sp <35611218+kearney-sp@users.noreply.github.com>
2022-05-06 10:36:44 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='hls_nrt_utils',
       author='Sean Patrick Kearney',
       author_email='sean.patrick@hotmail.com',
       license='MIT',
-      packages=find_packages(where='src', include=['src*', 'src/hls_funcs']),
+      packages=find_packages(where='hls_nrt_utils/', include=['src*']),
       package_dir={"": "src"},
       install_requires=['cartopy','certifi','cvxopt','dask','geopandas','numpy','pandas','rasterio','rioxarray','scikit-image','scikit-learn','scipy','stackstac','statsmodels','xarray','pysptools']
     )
