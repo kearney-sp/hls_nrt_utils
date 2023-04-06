0b0692d28f80ab3f5f77afc23060d7d9c8e99fed
kearney-sp <35611218+kearney-sp@users.noreply.github.com>
2023-04-06 11:02:20 -0700

    Update load.py

--- src/models/load.py
+++ src/models/load.py
@@ -1,8 +1,8 @@
 import pickle
 
 model_dict = {
-              'cper_biomass': './CPER_HLS_to_VOR_biomass_model_lr_simp.pk',
-              'cper_cover': './CPER_HLS_to_LPI_cover_pls_binned_model.pk'
+              'cper_biomass': 'CPER_HLS_to_VOR_biomass_model_lr_simp.pk',
+              'cper_cover': 'CPER_HLS_to_LPI_cover_pls_binned_model.pk'
               }
 
 def load_model(model_name):
