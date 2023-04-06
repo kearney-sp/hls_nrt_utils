85e90d4a00a77514e2711fdb22e7467221b3dced
kearney-sp <35611218+kearney-sp@users.noreply.github.com>
2023-04-06 11:08:22 -0700

    Update load.py

--- src/models/load.py
+++ src/models/load.py
@@ -2,8 +2,8 @@ import pickle
 from pkg_resources import resource_filename
 
 model_dict = {
-              'cper_biomass': resource_filename('models', 'CPER_HLS_to_VOR_biomass_model_lr_simp.pk'),
-              'cper_cover': resource_filename('models', 'CPER_HLS_to_LPI_cover_pls_binned_model.pk')
+              'cper_biomass': resource_filename('src', 'models/CPER_HLS_to_VOR_biomass_model_lr_simp.pk'),
+              'cper_cover': resource_filename('src', 'models/CPER_HLS_to_LPI_cover_pls_binned_model.pk')
               }
 
 def load_model(model_name):
