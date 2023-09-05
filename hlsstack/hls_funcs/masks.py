201103c924f699a7e2c60bfcab7da27147b9e9df
kearney-sp <35611218+kearney-sp@users.noreply.github.com>
2023-09-05 10:13:48 -0700

    Update masks.py to add numpy version of bolton_mask

--- hlsstack/hls_funcs/masks.py
+++ hlsstack/hls_funcs/masks.py
@@ -56,7 +56,95 @@ def mask_hls(da, mask_types=['all']):
     else:
         print('ERROR in "mask_types" definition. mask_types must be a list and one of, "all", "cirrus", "cloud", "cloud_adj", "snow", "water".')
 
-def bolton_mask(ds, time_dim='time'):
+def bolton_mask_np(blue, swir2):
+
+    indices = blue.index
+
+    ts_blue = blue.values
+    ts_swir2 = swir2.values
+    
+    def cloud_outlier_mask(da_blue):
+        blue_ts = da_blue / 10000.0
+        cloud_mask = np.zeros_like(blue_ts)
+        for idx in range(len(blue_ts)):
+            if not np.isnan(blue_ts[idx]):
+                idx_clear = np.where(~np.isnan(blue_ts))[0]
+                if idx == np.min(idx_clear):
+                    continue
+                else:
+                    idx_pre = np.max(idx_clear[idx_clear < idx])
+                    blue_diff = blue_ts[idx] - blue_ts[idx_pre]
+                    cloud_thresh = 0.03 * (1 + (idx - idx_pre) / 30)
+                    if blue_diff > cloud_thresh:
+                        blue_ts[idx] = np.nan
+                        cloud_mask[idx] = 1
+                    else:
+                        continue
+            else:
+                continue
+        return cloud_mask
+
+    def shadow_outlier_mask(da_swir2):
+        swir2_ts = da_swir2.copy()
+        shadow_mask = np.zeros_like(swir2_ts)
+        for idx in range(len(swir2_ts)):
+            if not np.isnan(swir2_ts[idx]):
+                idx_clear = np.where(~np.isnan(swir2_ts))[0]
+                if idx == np.min(idx_clear):
+                    continue
+                elif idx == np.max(idx_clear):
+                    try:
+                        idx_pre = idx_clear[idx_clear < idx][-1]
+                        idx_pre2 = idx_clear[idx_clear < idx][-2]
+                        y = np.array([swir2_ts[idx_pre2], swir2_ts[idx_pre]])
+                        x = np.array([idx_pre2, idx_pre])
+                        dx = np.diff(x)
+                        dy = np.diff(y)
+                        slope = dy / dx
+                        swir2_interp = swir2_ts[idx_pre] + slope[0] * (idx - idx_pre)
+                        swir2_diff = swir2_interp - swir2_ts[idx]
+                        if (swir2_ts[idx_pre] - swir2_ts[idx_pre2]) == 0:
+                            shadow_val = np.nan
+                        else:
+                            shadow_val = swir2_diff / (swir2_ts[idx_pre] - swir2_ts[idx_pre2])
+                        if (idx - idx_pre2 < 45) & (swir2_diff > 500) & (np.abs(shadow_val) > 2):
+                            swir2_ts[idx] = np.nan
+                            shadow_mask[idx] = 1
+                        else:
+                            continue
+                    except IndexError:
+                        continue
+                else:
+                    idx_pre = idx_clear[idx_clear < idx][-1]
+                    idx_post = idx_clear[idx_clear > idx][0]
+                    y = np.array([swir2_ts[idx_pre], swir2_ts[idx_post]])
+                    x = np.array([idx_pre, idx_post])
+                    dx = np.diff(x)
+                    dy = np.diff(y)
+                    slope = dy / dx
+                    swir2_interp = swir2_ts[idx_pre] + slope[0] * (idx - idx_pre)
+                    swir2_diff = swir2_interp - swir2_ts[idx]
+                    if (swir2_ts[idx_post] - swir2_ts[idx_pre]) == 0:
+                        shadow_val = np.nan
+                    else:
+                        shadow_val = swir2_diff / (swir2_ts[idx_post] - swir2_ts[idx_pre])
+                    if (idx_post - idx_pre < 45) & (swir2_diff > 500) & (np.abs(shadow_val) > 2):
+                        swir2_ts[idx] = np.nan
+                        shadow_mask[idx] = 1
+                    else:
+                        continue
+            else:
+                continue
+        return shadow_mask
+
+    shadow_outliers = shadow_outlier_mask(ts_swir2)
+    ts_blue[shadow_outliers == 1]= np.nan
+    cloud_outliers = cloud_outlier_mask(ts_blue)
+    mask = np.maximum(cloud_outliers, shadow_outliers)
+    mask = pd.Series(mask, index=indices)
+    return mask
+
+def bolton_mask_xr(ds, time_dim='time'):
     from hlsstack.hls_funcs.bands import blue_func, swir2_func
     dat_blue = blue_func(ds)
     dat_swir2 = swir2_func(ds)
