be99c6597a0a2ba43dc794e55814b91422b3f36c
kearney-sp <35611218+kearney-sp@users.noreply.github.com>
2023-09-12 14:34:15 -0700

    Update indices.py with additional vegetation indices
    Added all remaining indices from Jansen et al. 2018. Trying to address reduced performance of biomass model in summer.

--- hlsstack/hls_funcs/indices.py
+++ hlsstack/hls_funcs/indices.py
@@ -9,6 +9,7 @@ wav_SWIR2 = 2290 - (2290 - 2110) / 2
 
 
 def ndvi_func(src):
+    # Normalized Difference Vegetation Index
     band_RED = src['RED'].where(src['RED'] != -9999)
     band_NIR = src['NIR1'].where(src['NIR1'] != -9999)
     ndvi = (band_NIR - band_RED) / (band_NIR + band_RED)
@@ -17,6 +18,7 @@ def ndvi_func(src):
 
 
 def dfi_func(src):
+    # Dead Fuels Index
     band_RED = src['RED'].where(src['RED'] != -9999)
     band_NIR = src['NIR1'].where(src['NIR1'] != -9999)
     band_SWIR1 = src['SWIR1'].where(src['SWIR1'] != -9999)
@@ -26,6 +28,7 @@ def dfi_func(src):
 
 
 def ndti_func(src):
+    # Normalized Difference Tillage Index
     band_SWIR1 = src['SWIR1'].where(src['SWIR1'] != -9999)
     band_SWIR2 = src['SWIR2'].where(src['SWIR2'] != -9999)
     ndti = (band_SWIR1 - band_SWIR2) / (band_SWIR1 + band_SWIR2)
@@ -33,21 +36,126 @@ def ndti_func(src):
     return ndti
 
 
+def savi_func(src):
+    # Soil Adjusted Vegetation Index
+    band_RED = src['RED'].where(src['RED'] != -9999) * 0.0001
+    band_NIR = src['NIR1'].where(src['NIR1'] != -9999) * 0.0001
+    savi = ((band_NIR - band_RED) /
+             (band_NIR + band_RED + 0.5)) * (1 + 0.5)
+    return savi
+
+
 def satvi_func(src):
-    band_RED = src['RED'].where(src['RED'] != -9999)
-    band_SWIR1 = src['SWIR1'].where(src['SWIR1'] != -9999)
-    band_SWIR2 = src['SWIR2'].where(src['SWIR2'] != -9999)
-    satvi = ((band_SWIR1 * 0.0001 - band_RED * 0.0001) /
-             (band_SWIR1 * 0.0001 + band_RED * 0.0001 + 0.5)) * (1 + 0.5) - (band_SWIR2 * 0.0001 / 2)
+    # Soil Adjustted Total Vegetation Index
+    band_RED = src['RED'].where(src['RED'] != -9999) * 0.0001
+    band_SWIR1 = src['SWIR1'].where(src['SWIR1'] != -9999) * 0.0001
+    band_SWIR2 = src['SWIR2'].where(src['SWIR2'] != -9999) * 0.0001
+    satvi = ((band_SWIR1 - band_RED) /
+             (band_SWIR1 + band_RED + 0.5)) * (1 + 0.5) - (band_SWIR2 / 2.0)
     return satvi
 
 
 def ndii7_func(src):
+    # Normalized Difference Infrared Index 7 or Normalized Burn Ratio
     band_NIR = src['NIR1'].where(src['NIR1'] != -9999)
     band_SWIR2 = src['SWIR2'].where(src['SWIR2'] != -9999)
     ndii7 = (band_NIR - band_SWIR2) / (band_NIR + band_SWIR2)
     return ndii7
 
+
+def ndwi_func(src):
+    # Normalized Difference Water Index
+    band_NIR = src['NIR1'].where(src['NIR1'] != -9999)
+    band_SWIR1 = src['SWIR1'].where(src['SWIR1'] != -9999)
+    ndwi = (band_NIR - band_SWIR1) / (band_NIR + band_SWIR1)
+    return ndwi
+
+
+def evi_func(src):
+    # Enhanced Vegetation Index
+    band_BLUE = src['BLUE'].where(src['BLUE'] != -9999) * 0.0001
+    band_RED = src['RED'].where(src['RED'] != -9999) * 0.0001
+    band_NIR = src['NIR1'].where(src['NIR1'] != -9999) * 0.0001
+    evi = 2.5 * ((band_NIR - band_RED) / (band_NIR + 6*band_RED - 7.5*band_BLUE + 1))
+    return evi
+
+def tcbi_func(src):
+    # Tassled Cap Brightness Index
+    band_BLUE = src['BLUE'].where(src['BLUE'] != -9999) * 0.0001
+    band_GREEN = src['GREEN'].where(src['GREEN'] != -9999) * 0.0001
+    band_RED = src['RED'].where(src['RED'] != -9999) * 0.0001
+    band_NIR = src['NIR1'].where(src['NIR1'] != -9999) * 0.0001
+    band_SWIR1 = src['SWIR1'].where(src['SWIR1'] != -9999) * 0.0001
+    band_SWIR2 = src['SWIR2'].where(src['SWIR2'] != -9999) * 0.0001
+    tcbi = 0.2043*band_BLUE + 0.4158*band_GREEN + 0.5524*band_RED + 0.5741*band_NIR + 0.3124*band_SWIR1 + 0.2303*band_SWIR2
+    return tcbi
+
+
+def tcgi_func(src):
+    # Tassled Cap Greenness Index
+    band_BLUE = src['BLUE'].where(src['BLUE'] != -9999) * 0.0001
+    band_GREEN = src['GREEN'].where(src['GREEN'] != -9999) * 0.0001
+    band_RED = src['RED'].where(src['RED'] != -9999) * 0.0001
+    band_NIR = src['NIR1'].where(src['NIR1'] != -9999) * 0.0001
+    band_SWIR1 = src['SWIR1'].where(src['SWIR1'] != -9999) * 0.0001
+    band_SWIR2 = src['SWIR2'].where(src['SWIR2'] != -9999) * 0.0001
+    tcgi = -0.1603*band_BLUE - 0.2819*band_GREEN - 0.4934*band_RED + 0.7940*band_NIR - 0.0002*band_SWIR1 - 0.1446*band_SWIR2
+    return tcgi
+
+
+def tcwi_func(src):
+    # Tassled Cap Wetness Index
+    band_BLUE = src['BLUE'].where(src['BLUE'] != -9999) * 0.0001
+    band_GREEN = src['GREEN'].where(src['GREEN'] != -9999) * 0.0001
+    band_RED = src['RED'].where(src['RED'] != -9999) * 0.0001
+    band_NIR = src['NIR1'].where(src['NIR1'] != -9999) * 0.0001
+    band_SWIR1 = src['SWIR1'].where(src['SWIR1'] != -9999) * 0.0001
+    band_SWIR2 = src['SWIR2'].where(src['SWIR2'] != -9999) * 0.0001
+    tcwi = 0.0315*band_BLUE + 0.2021*band_GREEN + 0.3102*band_RED + 0.1594*band_NIR - 0.6806*band_SWIR1 -0.6109*band_SWIR2
+    return tcwi
+
+
+def psri_func(src):
+    # Plant Senesence Reflectance Index
+    band_GREEN = src['GREEN'].where(src['GREEN'] != -9999)
+    band_RED = src['RED'].where(src['RED'] != -9999)
+    band_NIR = src['NIR1'].where(src['NIR1'] != -9999)
+    psri = (band_RED - band_GREEN) / band_NIR
+    return psri
+
+
+def mtvi1_func(src):
+    # Modified Triangular Vegetation Index 1
+    band_GREEN = src['GREEN'].where(src['GREEN'] != -9999) * 0.0001
+    band_RED = src['RED'].where(src['RED'] != -9999) * 0.0001
+    band_NIR = src['NIR1'].where(src['NIR1'] != -9999) * 0.0001
+    mtvi1 = 1.2*(1.2*(band_NIR - band_GREEN) - 2.5*(band_RED - band_GREEN))
+    return mtvi1
+
+
+def nci_func(src):
+    # Normalized Canopy Index
+    band_GREEN = src['GREEN'].where(src['GREEN'] != -9999)
+    band_SWIR1 = src['SWIR1'].where(src['SWIR1'] != -9999)
+    nci = (band_SWIR1 - band_GREEN) / (band_SWIR1 + band_GREEN)
+    return nci
+
+
+def ndci_func(src):
+    # Normalized Difference Cover Index or Normalized Difference Senescent Vegetation Index
+    band_RED = src['RED'].where(src['RED'] != -9999)
+    band_SWIR1 = src['SWIR1'].where(src['SWIR1'] != -9999)
+    ndci = (band_SWIR1 - band_RED) / (band_SWIR1 + band_RED)
+    return ndci
+
+
+def rdvi_func(src):
+    # Renormalized Difference Vegetation Index
+    band_RED = src['RED'].where(src['RED'] != -9999)
+    band_NIR = src['NIR1'].where(src['NIR1'] != -9999)
+    rdvi = (band_NIR - band_RED) / np.sqrt(band_NIR + band_RED)
+    return rdvi
+    
 
 def bai_126_func(src):
     band_BLUE = src['BLUE'].where(src['BLUE'] != -9999)
