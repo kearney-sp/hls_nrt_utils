67d781f94d611ee59f006ff97e7368694ce588d7
Sean Kearney <sean.kearney@usda.gov>
2024-10-10 13:40:27 -0500

    Add PLS Biomass model for CPER and make default. Old model available as biomass_old.

--- hlsstack/models/load.py
+++ hlsstack/models/load.py
@@ -2,9 +2,10 @@ import pickle
 from pkg_resources import resource_filename
 
 model_dict = {
-              'cper_biomass': resource_filename('hlsstack', 'models/CPER_HLS_to_VOR_biomass_model_lr_simp.pk'),
-              'cper_cover': resource_filename('hlsstack', 'models/CPER_HLS_to_LPI_cover_pls_binned_model.pk')
-              }
+    'cper_biomass': resource_filename('hlsstack', 'models/CPER_VOR_to_biomass_model_20240325.pk'),
+    'cper_cover': resource_filename('hlsstack', 'models/CPER_HLS_to_LPI_cover_pls_binned_model.pk'),
+    'cper_biomass_old': resource_filename('hlsstack', 'models/CPER_HLS_to_VOR_biomass_model_lr_simp.pk')
+}
 
 def load_model(model_name):
    model = pickle.load(open(model_dict[model_name], 'rb'))
