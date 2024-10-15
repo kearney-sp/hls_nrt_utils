9db3a801f568be94c651a236f6b71d9bd0d0339a
Sean Kearney <sean.kearney@usda.gov>
2024-10-15 12:43:50 -0500

    Trying to fix biomass model update (PLS). Pickled new model without ml_setup dependency.

--- hlsstack/models/load.py
+++ hlsstack/models/load.py
@@ -2,7 +2,7 @@ import pickle
 from pkg_resources import resource_filename
 
 model_dict = {
-    'cper_biomass': resource_filename('hlsstack', 'models/CPER_HLS_to_VOR_biomass_model_pls_20241010.pk'),
+    'cper_biomass': resource_filename('hlsstack', 'models/CPER_HLS_to_VOR_biomass_model_pls_20241015.pk'),
     'cper_cover': resource_filename('hlsstack', 'models/CPER_HLS_to_LPI_cover_pls_binned_model.pk'),
     'cper_biomass_old': resource_filename('hlsstack', 'models/CPER_HLS_to_VOR_biomass_model_lr_simp.pk')
 }
