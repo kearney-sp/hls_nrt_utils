47e39ccee104692d1c5e011798725d32b9dcbec6
Sean Kearney <sean.kearney@usda.gov>
2025-02-05 14:38:54 -0600

    Update with newly pickled cover model. From SCINet SPK Atlas.

--- hlsstack/models/load.py
+++ hlsstack/models/load.py
@@ -3,7 +3,8 @@ from pkg_resources import resource_filename
 
 model_dict = {
     'cper_biomass': resource_filename('hlsstack', 'models/CPER_HLS_to_VOR_biomass_model_pls_20241015.pk'),
-    'cper_cover': resource_filename('hlsstack', 'models/CPER_HLS_to_LPI_cover_pls_binned_model.pk'),
+    'cper_cover': resource_filename('hlsstack', 'models/CPER_HLS_to_LPI_cover_model_20250205.pk'),
+    'cper_cover_old': resource_filename('hlsstack', 'models/CPER_HLS_to_LPI_cover_pls_binned_model.pk'),
     'cper_biomass_old': resource_filename('hlsstack', 'models/CPER_HLS_to_VOR_biomass_model_lr_simp.pk')
 }
 
